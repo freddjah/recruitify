0f41baed178533707ed9a4ee00997fce35d073a4
Erik <>
2019-01-31 17:17:42 +0100

    Started validation for application

--- app/Controllers/Http/Inside/Applicant/ApplicationController.js
+++ app/Controllers/Http/Inside/Applicant/ApplicationController.js
@@ -2,6 +2,18 @@
 
 const CompetenceRepository = use('App/Repositories/CompetenceRepository')
 
+function validateSave(data) {
+
+  const errors = {}
+
+  // Validate expertises
+  if (!data.expertise_competence_id || !data.expertise_years_of_experience || data.expertise_competence_id.length === 0 || data.expertise_years_of_experience.length === 0) {
+    errors.expertises = 'Enter at least one expertise'
+  }
+
+  return errors
+}
+
 class ApplicationController {
 
   async applicationForm({ view }) {
@@ -10,8 +22,18 @@ class ApplicationController {
     return view.render('inside.applicant.application.application-form', { competences })
   }
 
-  saveApplication({ request, response }) {
-    console.log(request.all())
+  saveApplication({ request, response, session }) {
+
+    const errors = validateSave(request.all())
+    console.log('errors', errors)
+
+    if (Object.keys(errors).length > 0) {
+
+      session.withErrors(errors).flashAll()
+      return response.redirect('back')
+    }
+
+    // console.log(request.all())
     return response.send('Success')
   }
 }
