87f84df05ba03a4f7b5240d8d9418e004cc69933
Erik Holm <erik.gholm@gmail.com>
2019-02-13 19:03:49 +0100

    Unit test for HomeController

---
+++ test/unit/Controllers/Http/HomeController.spec.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const sinon = require('sinon')
+const clearDatabase = require('../../../clearDatabase')
+
+const { test, beforeEach } = use('Test/Suite')('AuthenticationController')
+const HomeController = use('App/Controllers/Http/HomeController')
+
+const response = { send: sinon.spy(), redirect: sinon.spy() }
+const controller = new HomeController()
+
+beforeEach(async () => {
+
+  await clearDatabase()
+  response.send.resetHistory()
+})
+
+test('home: Renders the home page for specific role', async () => {
+
+  const auth = {
+    getUser: () => ({
+      role: () => ({
+        fetch: () => ({
+          name: 'applicant',
+        }),
+      }),
+    }),
+  }
+  await controller.home({ auth, response })
+  sinon.assert.calledWith(response.redirect, `/${auth.getUser().role().fetch().name}`)
+})
