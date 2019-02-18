c7c7e8fb5d25922f0ae5cb21f1b86d299fdd1799
Erik Holm <erik.gholm@gmail.com>
2019-02-18 16:03:52 +0100

    Added tables to applicant application page

--- public/javascript/applicant/application.js
+++ public/javascript/applicant/application.js
@@ -3,12 +3,15 @@ $(() => {
   $('#add_expertise').click(event => {
 
     event.preventDefault()
+
+    $('#expertises .empty-state').remove()
+
     const competenceId = $('#competence').val()
     const competenceName = $('#competence option:selected').text()
 
     const yearsOfExperience = $('#years_of_experience').val()
 
-    $('#expertises').append(`<li>${competenceName} - ${yearsOfExperience} years</li>`)
+    $('#expertises').append(`<tr> <td>${competenceName}</td> <td>${yearsOfExperience} years</td> </tr>`)
     $('form').append(`<input type="hidden" name="expertiseCompetenceId[]" value="${competenceId}">`)
     $('form').append(`<input type="hidden" name="expertiseYearsOfExperience[]" value="${yearsOfExperience}">`)
   })
@@ -16,10 +19,13 @@ $(() => {
   $('#add_availability').click(event => {
 
     event.preventDefault()
+
+    $('#availabilities .empty-state').remove()
+
     const availabilityFrom = $('#availability_from').val()
     const availabilityTo = $('#availability_to').val()
 
-    $('#availabilities').append(`<li>${availabilityFrom} to ${availabilityTo}</li>`)
+    $('#availabilities').append(`<tr> <td>${availabilityFrom}</td> <td>${availabilityTo}</td> </tr>`)
     $('form').append(`<input type="hidden" name="availabilityFrom[]" value="${availabilityFrom}">`)
     $('form').append(`<input type="hidden" name="availabilityTo[]" value="${availabilityTo}">`)
   })
